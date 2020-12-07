e315acfe9137bc3a7234898e6ead6f9436cbea34
tom1k <tomascosta@ua.pt>
2020-12-07 00:02:33 +0000

    Added graph generation

--- code/main.py
+++ code/main.py
@@ -1,17 +1,18 @@
 from graph import Graph
 import sys
 import getopt
+import random
 
 def usage():
-    print("Usage: python3 main.py \n\t-w <write results to file: no argument>\n\t-g <generate random graph with N vertices: int>")
+    print("Usage: python3 main.py \n\t-t <test more graphs with given solutions: no argument>\n\t-g <generate random graph with N vertices: int>")
 
 if __name__ == "__main__":
     # initialization of random variables
     num_vertices = None
-    write_flag = False
+    testing_flag = False
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hwg:", ["help", "output="])
+        opts, args = getopt.getopt(sys.argv[1:], "htg:", ["help", "output="])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(str(err))  # will print something like "option -a not recognized"
@@ -23,19 +24,38 @@ if __name__ == "__main__":
         if o in ("-h", "--help"):
             usage()
             sys.exit()
-        elif o == "-w":
-            write_flag = True
+        elif o == "-t":
+            testing_flag = True
         elif o == "-g":
             try:
                 num_vertices = int(a)
-                assert int(a)>1, "chunksize bigger than 1"
+                assert int(a)>1, "Number of vertices bigger than 1"
             except:
                 usage()
                 sys.exit()
 
     # Call in random graph generator
     if num_vertices != None:
-        pass
+        graph = Graph(num_vertices)
+        for i in range(0,num_vertices):
+            n_iters = random.choice([x for x in range(0,num_vertices)])
+            used_vertices = []
+            for iteration in range(0, n_iters):
+                j = list( set([ x for x in range(num_vertices) if x != i]) - set(used_vertices))
+                j = random.choice(j)
+                print("Creating edge: G[%d][%d]" %  (i,j))
+                graph.add_edge(i,j)
+                used_vertices.append(j)
+
+        result = graph.color_matrix()
+        print("Chromatic Index calculated correctly? %s\n" % (3==result))
+
+        # Available functions for visualizing matrixes
+        print("Analyzing graph:")
+        graph.print_adj_matrix()    
+        graph.print_colors_matrix()   
+
+    # Testing mode - OPTARG
     # Test the scenarios we know are working
     else:
         # Graph Creation and definition, by adding edges
@@ -46,37 +66,31 @@ if __name__ == "__main__":
         graph.add_edge(1,2)
         graph.add_edge(2,3)
 
-        result = graph.color_matrix()
-        print("Chromatic Index calculated correctly? %s\n" % (3==result))
 
+        if testing_flag:
+            print("\n---------------------------------------------------\nConducting extra tests:\n")
+            # Test nº 2
+            graph = Graph(5)
+            graph.add_edge(0,1)
+            graph.add_edge(0,2)
+            graph.add_edge(0,3)
+            graph.add_edge(0,4)
 
-        # Test nº 2
-        graph = Graph(5)
-        graph.add_edge(0,1)
-        graph.add_edge(0,2)
-        graph.add_edge(0,3)
-        graph.add_edge(0,4)
+            graph.add_edge(1,2)
+            graph.add_edge(1,4)
 
-        graph.add_edge(1,2)
-        graph.add_edge(1,4)
+            graph.add_edge(2,3)
 
-        graph.add_edge(2,3)
+            graph.add_edge(0,2)
 
-        graph.add_edge(0,2)
+            result = graph.color_matrix()
+            print("Chromatic Index calculated correctly? %s\n" % (4==result))
 
-        result = graph.color_matrix()
-        print("Chromatic Index calculated correctly? %s\n" % (4==result))
+            # Test nº 3
+            graph = Graph(3)
+            graph.add_edge(0,1)
+            graph.add_edge(0,2)
+            graph.add_edge(1,2)
 
-        # Test nº 3
-        graph = Graph(3)
-        graph.add_edge(0,1)
-        graph.add_edge(0,2)
-        graph.add_edge(1,2)
-
-        result = graph.color_matrix()
-        print("Chromatic Index calculated correctly? %s\n" % (3==result))
-
-        # Available functions for visualizing matrixes
-        print("Analyzing last graph:")
-        graph.print_adj_matrix()    
-        graph.print_colors_matrix()   
+            result = graph.color_matrix()
+            print("Chromatic Index calculated correctly? %s\n" % (3==result))
